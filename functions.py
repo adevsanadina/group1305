3d6e093b74ae1c12377fb9a1c7eb1f55a010e206
v win10 <you@example.com>
2024-06-05 12:54:34 +0300

    first functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+def generate_greeting(username):
+    return f'<h1> Вітаємо тебе, шановний {username} </h1>'
+
+# Exmple
+username = "Legend"
+
+print(generate_greeting(username))
+
+def rectangle_area(length, width):
+    return length * width
+
+# Example
+length = 3
+width = 7
+
+print(rectangle_area(length, width))
